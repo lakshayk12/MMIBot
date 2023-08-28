83ac8a12c14405c4a6422566b3f0957b6f3e946a
lakshayk12 <lakshayk12@yahoo.com>
2023-08-28 21:18:48 +0530

    Using BeautifulSoup to parse MMI Website

--- bot.py
+++ bot.py
@@ -1,46 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import requests
 from alert import Alert
 
 
 class Bot:
 
-    def __init__(self, url, email, name):
+    def __init__(self, url, client_email):
         self.url = url
-        self.email = email
-        self.name = name
+        self.client_email = client_email
 
-    def check_product_by_url(self):
-        # browser settings
-        options = Options()
-        options.headless = True
-        options.add_experimental_option("detach", True)
+    def check_market_mood(self):
+        # Send an HTTP GET request to the webpage
+        response = requests.get(self.url)
 
-        # browser init and call product url
-        browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-        browser.maximize_window()
-        browser.get(self.url)
+        # Create a BeautifulSoup object with the page content
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        # loop until article is available
-        while True:
-            try:
-                # check if buy button is available with 10 sec page loading time
-                availability_div = WebDriverWait(browser, 10).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'number'))
-                )
-
-                if availability_div.text.lower():
-                    print("MMI is fetched:", availability_div.text.lower())
-                    # send email notification
-                    Alert.email_alert(browser.title + " is available !!!", browser.current_url, self.email)
-                    browser.quit()
-                    break
-                # browser.execute_script("location.reload(true);")
-            except:
-                # reload browser and try again
-                browser.execute_script("location.reload(true);")
+        try:
+            mmi_number = soup.find(class_='number').getText()
+            print("Market Mood Index:", mmi_number)
+        except:
+            print("Try Again!")
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # replace URL-Text with product that should be checked on Amazon
     # replase EMAIL-Text where the notification should be sent
     url = "https://www.tickertape.in/market-mood-index"
-    email = "imcoollakshay02@gmail.com"
-    amazon_bot = Bot(url, email, "<PRODUCT NAME>")
-    amazon_bot.check_product_by_url()
-    amazon_bot.check_product_by_name()
+    client_email = "imcoollakshay02@gmail.com"
+
+    mmi_bot = Bot(url, client_email)
+    mmi_bot.check_market_mood()
